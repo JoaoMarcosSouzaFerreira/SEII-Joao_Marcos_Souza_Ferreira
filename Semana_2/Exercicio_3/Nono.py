edf695f103b9961dd982d4ce84407d2cdba66a74
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-01-26 20:48:18 -0300

    Create Nono.py

---
+++ Semana_2/Exercicio_3/Nono.py
@@ -0,0 +1,17 @@
+temperatura = 35
+if temperatura >= 30:
+    print("É um dia quente")
+    print("Beba muita água")
+print("Pronto")
+
+temperatura = 25
+if temperatura >= 30:
+    print("É um dia quente")
+    print("Beba muita água")
+elif temperatura > 20:
+    print("Temperatura agradável")
+elif temperatura > 10:
+    print("Está um pouco frio")
+else:
+    print("Está frio")
+print("Pronto")
