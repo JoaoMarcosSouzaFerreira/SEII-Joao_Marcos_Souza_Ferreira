a311821c531742a69aaf67d252e95196a4ce66fc
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-01-26 20:38:50 -0300

    Create Terceiro.py

---
+++ Semana_2/Exercicio_3/Terceiro.py
@@ -0,0 +1,10 @@
+aniversario = input("Insira o ano de seu aniversário: ")
+idade = 2024 - int(aniversario)
+print(idade)
+
+------------------------------------------------------------
+
+x = input("Primeiro: ")
+y = input("Segundo: ")
+soma = float(x) + float(y)
+print("Soma: " + str(sum))
