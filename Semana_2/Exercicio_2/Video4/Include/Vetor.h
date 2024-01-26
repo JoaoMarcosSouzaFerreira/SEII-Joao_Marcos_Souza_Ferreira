4f035c3c3f8d19443a99ea387ef3a80767b30da3
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-01-26 20:23:19 -0300

    Create Vetor.h

---
+++ Semana_2/Exercicio_2/Video4/Include/Vetor.h
@@ -0,0 +1,18 @@
+#ifndef FLOAT_VECTOR_H
+#define FLOAT_VECTOR_H
+
+
+typedef struct float_vector FloatVector;
+
+FloatVector *FloatVector_create(int capacity);
+void FloatVector_detroy(FloatVector **vec_ref);
+int FloatVector_size(const FloatVector *vec);
+int FloatVector_capacity(const FloatVector *vec);
+float FloatVector_at(const FloatVector *vec, int index);
+float FloatVector_get(const FloatVector *vec, int index);
+void FloatVector_append(FloatVector *vec, float val);
+float FloatVector_remove(FloatVector *vec, int index);
+void FloatVector_set(FloatVector *vec, int index, float val);
+void FloatVector_print(const FloatVector *vec);
+
+#endif
