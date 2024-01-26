ce8c79c9d5f2fe5630dded55e53a0a50675c4383
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-01-26 20:41:11 -0300

    Create Quarto.py

---
+++ Semana_2/Exercicio_3/Quarto.py
@@ -0,0 +1,8 @@
+curso = "Python for beginners"
+print(curso.upper())
+print(curso.lower())
+print(curso.find('y'))
+print(curso.find('Y'))
+print(curso.replace('for', '4'))
+print(curso.replace('x', '4'))
+print('Python' in curso)
