f06bd042e29af55504b075b2804c74aba36dc035
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-01-26 20:28:35 -0300

    Create MyTime.c

---
+++ Semana_2/Exercicio_2/Video4/Src/MyTime.c
@@ -0,0 +1,20 @@
+#include "mytime.h"
+
+
+timer tic() {
+    timer tic_;
+    gettimeofday(&tic_, NULL); // "marca" o tempo atual
+    return tic_;
+}
+
+
+timer tac() {
+    return tic();
+}
+
+float comptime(timer tic, timer tac) {
+  float t = ((tac.tv_sec  - tic.tv_sec) * 1000.0) +
+            ((tac.tv_usec - tic.tv_usec) * 0.001);
+  
+  return t;
+}
