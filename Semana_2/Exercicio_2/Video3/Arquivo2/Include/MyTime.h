86077ddba96f6f64085dd6de8e0fbdb34a32ecd6
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-01-26 20:00:29 -0300

    Create MyTime.h

---
+++ Semana_2/Exercicio_2/Video3/Arquivo2/Include/MyTime.h
@@ -0,0 +1,14 @@
+#ifndef MY_TIME_H
+#define MY_TIME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+typedef struct timeval timer; 
+timer tic();
+timer tac();
+
+float comptime(timer tic, timer tac);
+
+#endif
