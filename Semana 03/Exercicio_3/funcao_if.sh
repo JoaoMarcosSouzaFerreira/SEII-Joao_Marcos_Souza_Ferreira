e4e9bec8507ede10892ef1cf7c3b5946f96b4fd6
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-01-30 11:08:31 -0300

    Create funcao_if.sh

---
+++ Semana 03/Exercicio_3/funcao_if.sh
@@ -0,0 +1,7 @@
+if [ ${1,,} = herbert ]; then
+	echo "Oh, you're the boss here. Welcome!"
+elif [ ${1,,} = help ]; then
+	echo "Just enter your username, duh!"
+else
+	echo "I don't know who you are. But you're not the boss of me!"
+fi
