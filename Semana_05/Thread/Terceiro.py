577be9dbb6d7c3912e6fd8e6c496f9c8a8772353
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-02-11 20:30:21 -0300

    Terceiro.py

---
+++ Semana_05/Thread/Terceiro.py
@@ -0,0 +1,25 @@
+import threading
+import time
+
+start = time.perf_counter()
+
+
+def do_something():
+  print('Sleeping 1 second...')
+  time.sleep(1)
+  print('Done Sleeping...')
+
+
+threads =[]
+for _ in range(10):
+  t = threading.Thread(target=do_something)
+  t.start()
+  threads.append(t)
+
+
+for thread in threads:
+  thread.join()
+
+finish= time.perf_counter()
+
+print(f'Finished in {round(finish-start,2)} second(s)')
