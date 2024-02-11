0f497f56cef59f186d13f6efb374587b9603fb5b
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-02-11 20:36:38 -0300

    Quinto.py

---
+++ Semana_05/Thread/Quinto.py
@@ -0,0 +1,18 @@
+import concurrent.futures
+import time
+
+start = time.perf_counter()
+
+
+def do_something(seconds):
+  print('Sleeping {seconds} second(s)...')
+  time.sleep(seconds)
+  return 'Done Sleeping...' 
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+  f1 = executor.sumbmit(do_something, 1)
+  print(f1.result())
+
+finish= time.perf_counter()
+
+print(f'Finished in {round(finish-start,2)} second(s)')
