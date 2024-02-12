f477af4592c39f5b273df1066830b8f4158c9d4b
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-02-11 21:04:23 -0300

    Segundo.py

---
+++ Semana_05/Multiprocess/Segundo.py
@@ -0,0 +1,25 @@
+import multiprocessing
+import time
+
+start = time.perf_counter()
+
+
+def do_something():
+  print('Sleeping 1 second...')
+  time.sleep(1)
+  print('Done sleeping...')
+
+
+processes = []
+
+for _ in range(10):
+  p = multiprocessing.Process(target=do_something)
+  p.start()
+  processes.append(p)
+
+for process in processes:
+  process.join()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start,2)} second(s)')
