c8cbf4fac5000fe2d7002466541c520d44e30eff
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-02-11 21:13:07 -0300

    Quinto.py

---
+++ Semana_05/Multiprocess/Quinto.py
@@ -0,0 +1,21 @@
+import concurrent.futures
+import time
+
+start = time.perf_counter()
+
+
+def do_something(seconds):
+  print(f'Sleeping {seconds} second(s)...')
+  time.sleep(seconds)
+  return 'Done sleeping...'
+
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+  f1 = executor.submit(do_something, 1)
+  f2 = executor.submit(do_something, 1)
+  print(f1.resut())
+  print(f2.resut())
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start,2)} second(s)')
