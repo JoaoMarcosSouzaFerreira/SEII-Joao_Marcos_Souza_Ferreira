ac40d3b4d3bf829682a9ce868ff4739849edc98f
João Marcos <157326995+JoaoMarcosSouzaFerreira@users.noreply.github.com>
2024-04-04 22:21:47 -0300

    Update hello.py

--- Semana_09/problema_02/hello.py
+++ Semana_09/problema_02/hello.py
@@ -1,4 +1,3 @@
-
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -8,29 +7,50 @@ from kivy.uix.textinput import TextInput
 
 class SayHello(App):
     def build(self):
+        #returns a window object with all it's widgets
         self.window = GridLayout()
-        #style
         self.window.cols = 1
-        self.window.size_hint= (0.6, 0.7)
-        self.window.pos_hint={"center_x":0.5, "center_y":0.5}
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y":0.5}
 
-        # Add widgets to window
-        self.window.add_widget(Image(source="download.png"))
-        self.greeting = Label(text="What's your name?",font_size=22, color='#00FFCE')
+        # image widget
+        self.window.add_widget(Image(source="logo.png"))
+
+        # label widget
+        self.greeting = Label(
+                        text= "What's your name?",
+                        font_size= 18,
+                        color= '#00FFCE'
+                        )
         self.window.add_widget(self.greeting)
-        self.user = TextInput(multiline=False,padding_y=(20,20),size_hint=(1,0.5))
+
+        # text input widget
+        self.user = TextInput(
+                    multiline= False,
+                    padding_y= (20,20),
+                    size_hint= (1, 0.5)
+                    )
+
         self.window.add_widget(self.user)
-        self.button = Button(text="GREET",size_hint=(1,0.5),bold=True,background_color='#0000FF')
-        self.button.bind(on_press= self.callback)
+
+        # button widget
+        self.button = Button(
+                      text= "GREET",
+                      size_hint= (1,0.5),
+                      bold= True,
+                      background_color ='#00FFCE',
+                      #remove darker overlay of background colour
+                      # background_normal = ""
+                      )
+        self.button.bind(on_press=self.callback)
         self.window.add_widget(self.button)
 
         return self.window
 
-
     def callback(self, instance):
-        self.greeting.text= "Hello " + self.user.text+ "!"
+        # change label text to "Hello + user name!"
+        self.greeting.text = "Hello " + self.user.text + "!"
 
-        
-
+# run Say Hello App Calss
 if __name__ == "__main__":
     SayHello().run()
